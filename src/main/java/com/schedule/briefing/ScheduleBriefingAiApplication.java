1d03344fdd80c67b07e743971076ca298d42fafb
Suho Lee <sh.lee@vcx.co.kr>
2026-07-13 15:44:38 +0900

    Initial commit: Spring Boot 프로젝트 셋업

---
+++ src/main/java/com/schedule/briefing/ScheduleBriefingAiApplication.java
@@ -0,0 +1,13 @@
+package com.schedule.briefing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleBriefingAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleBriefingAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schedule/briefing/ScheduleBriefingAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedule.briefing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleBriefingAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
